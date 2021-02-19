8413079b1865571de7cd76f67094b0ca7ea15ade
satish-shah <fireside.satish2012@gmail.com>
2021-02-19 15:01:35 +1100

    declaring the direction in all the direction along with vector

--- robot.py
+++ robot.py
@@ -5,4 +5,11 @@ command_list = data.split(",")
 
 print(command_list)
 
-#now declaring the moves array
+#now declaring the DIRECTION array
+
+current_pos=0
+direction={"F":1j,
+       "B":-1j,
+       "L":-1,
+       "R":1
+       }
