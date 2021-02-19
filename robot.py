6d9da3274026807c1fd502708b3d0489cd591af2
satish-shah <fireside.satish2012@gmail.com>
2021-02-19 15:06:13 +1100

    determining how many moves the robot have made and the distance travelled

--- robot.py
+++ robot.py
@@ -6,6 +6,7 @@ command_list = data.split(",")
 print(command_list)
 
 #now declaring the DIRECTION array
+#we have also given the vector for the move
 
 current_pos=0
 direction={"F":1j,
@@ -13,3 +14,12 @@ direction={"F":1j,
        "L":-1,
        "R":1
        }
+
+#we are determining the moves robot have made
+# robot on valid moves and the magnitude is calculated
+for command in command_list:
+    parts=split(command)    
+    move=parts[0]
+    value=parts[1]
+   
+    current_pos += direction[move]*int(value)
